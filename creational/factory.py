2cf3e0cfdd5bd2a4107e10e52353c08de33e0e26
Marcos Federico Mandrille <mmandrille@gmail.com>
2022-08-10 02:57:41 -0300

    initial working factory pattern

--- creational/factory.py
+++ creational/factory.py
@@ -22,62 +22,157 @@ is expected to return objects with no known common parent class, the factory can
 be implemented as a standalone function or within an independent `Factory` class.
 
 """
-
-from __future__ import generators
+# Python imports
+import abc
 import random
+# Package Imports
+import pygame
 
-class Parent(object):
-    """This class serves as the base class for all object types that the factory
-    can produce."""
+# Classes definition
+class AbstractFactory(object):
+    __metaclass__ = abc.ABCMeta
+    @abc.abstractmethod
+    def make_object(self):
+        return
 
-    def __init__(self, lifespan):
-        """In this example, we will set a lifespan attribute to the parent (inherited by
-        all its children). It is for demonstrative purposes only and is not pertinent
-        to the Factory Pattern."""
+class ShapeFactory(object):
+    def __init__(self, x, y, window_dimensions):
+        # Paramete5rs
+        self.window_dimensions = window_dimensions
+        # Horizontal Position
+        self.x = x
+        self.min_limit_x = 0
+        self.max_limit_x = window_dimensions[0] - self.width
+        # Vertical Position
+        self.y = y
+        self.min_limit_y = 0
+        self.max_limit_y = window_dimensions[1] - self.height
+        # Some feedback
+        print(f"Position: {self.x} x {self.y}")
+        print(f"Horizontal Limits: {self.min_limit_x} to {self.max_limit_x}")
+        print(f"Vertical Limits: {self.min_limit_y} to {self.max_limit_y}")
 
-        self.lifespan = lifespan
+    def draw(self):
+        raise NotImplementedError()
 
-    @staticmethod
-    def make_child(name, args):
-        """This is the factory method. It creates a layer of abstraction wherein
-        it can create and return objects based on some information about the object. In
-        this case we are simply supplying the name of the object type as string but you
-        could easily include more complex logic to derive the required object type based
-        on less direct information."""
+    def move(self, direction, speed=1, limit=False):
+        # Move circle
+        steps = 4 * speed
+        if direction == 'left':
+            self.x -= steps
+            if self.x < self.min_limit_x or limit:
+                self.x = self.min_limit_x
+        elif direction == 'right':
+            self.x += steps
+            if self.x > self.max_limit_x or limit:
+                self.x = self.max_limit_x
+        elif direction == 'up':
+            self.y -= steps
+            if self.y < self.min_limit_y or limit:
+                self.y = self.min_limit_y
+        elif direction == 'down':
+            self.y += steps
+            if self.y > self.max_limit_y or limit:
+                self.y = self.max_limit_y 
 
-        for Child in Parent.__subclasses__():
-            if Child.__name__ == name:
-                return Child(*args)
+    @staticmethod 
+    def build(type, window_dimensions):
+        shapeClass = ShapeDict.get(type)
+        if shapeClass:
+            return shapeClass(100, 100, window_dimensions)
+        # In case no match
+        assert 0, "Bad shape requested: " + type
 
-        return False
+    def transform(self):
+        random_shapeClass = random.choice([value for key, value in ShapeDict.items() if not key == self.shape])
+        return random_shapeClass(self.x, self.y, self.window_dimensions)
 
-class TypeAChild(Parent):
-    pass
+class Square(ShapeFactory):
+    def __init__(self, x, y , window_dimensions):
+        self.shape = "Square"
+        self.width = 20
+        self.height = 20
+        super().__init__(x, y , window_dimensions)
 
-class TypeBChild(Parent):
-    pass
+    def draw(self):
+        pygame.draw.rect(
+            screen,
+            (255, 255, 0),
+            pygame.Rect(self.x, self.y, self.width, self.height) # Position + Size
+        )
 
-def generate_children(count, parent, life_min, life_max):
-    """We are using a generator to randomly seed a sequence of specifications for the
-    factory. While this is not strictly part of the Factory Pattern, it can be useful
-    for testing the factory or for generating objects based on some pre-defined algorithm.
-    It is perfectly acceptable to implement factories without generators (depends on your
-    specific use-case)."""
+class Circle(ShapeFactory):
+    def __init__(self, x, y , window_dimensions):
+        self.shape = "Circle"
+        self.width = 10
+        self.height = self.width
+        super().__init__(x, y , window_dimensions)
 
-    types = parent.__subclasses__()
-    for i in range(count):
-        yield { 
-            "class_name": random.choice(types).__name__, 
-            "lifespan": random.randint(life_min, life_max)
-        }
+    def draw(self):
+        pygame.draw.circle(
+            screen,
+            (0, 255, 255),
+            (self.x, self.y), # Position
+            self.width # Size
+        )
+
+class Triangle(ShapeFactory):
+    def __init__(self, x, y , window_dimensions):
+        self.shape = "Triangle"
+        self.width = 10
+        self.height = self.width
+        super().__init__(x, y , window_dimensions)
+
+    def draw(self):
+        pygame.draw.polygon(
+            surface=screen,
+            color = (255, 0, 255),
+            points=[(self.x,self.y+25), (self.x,self.y-25), (self.x+25,self.y)]
+        )
+
+# Global Definition of Available Shapes
+ShapeDict = {
+    "Circle": Circle,
+    "Square": Square,
+    "Triangle": Triangle
+}
 
 if __name__ == '__main__':
+    window_dimensions = 800, 600
+    screen = pygame.display.set_mode(window_dimensions)
+    obj = ShapeFactory.build("Square", window_dimensions)
+    
+    player_quits = False
+    while not player_quits: # Keep doing till player press Exit
+        # Every time something happens:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT: # player press Exit
+                player_quits = True # End the while
 
-    print("Generating 10 children of 'Parent' with a random lifespan between 0 - 99:")
+            # Check player interaction            
+            elif event.type == pygame.KEYDOWN:
+                print(event)
+                pressed = pygame.key.get_pressed() # We fetch keystroke
+                
+                # In case of Shift Pressing:
+                limit = False
+                speed = 1
+                if  pygame.key.get_mods() & pygame.KMOD_SHIFT:
+                    speed = 5
+                
+                if pygame.key.get_mods() & pygame.KMOD_CTRL:
+                    limit = True
+                
+                # Get pressed key
+                if pressed[pygame.K_UP]: obj.move('up', speed, limit)
+                if pressed[pygame.K_DOWN]: obj.move('down', speed, limit)
+                if pressed[pygame.K_LEFT]: obj.move('left', speed, limit)
+                if pressed[pygame.K_RIGHT]: obj.move('right', speed, limit)
 
-    # The generator and factory work in cohesion to algorithmically generate objects.
-    children = [Parent.make_child(name=params["class_name"], args=[params["lifespan"]]) \
-        for params in generate_children(count=10, parent=Parent, life_min=0, life_max=99)]
-
-    for child in children:
-        print(type(child).__name__, child.lifespan)
+                # If want to transform:
+                if pressed[pygame.K_c]: # After pressing C
+                    obj = obj.transform()        
+        
+            screen.fill((0, 0, 0))
+            obj.draw() # Draw my object
+        pygame.display.flip()
