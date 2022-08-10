bd1a0b028a03b20c98b3e82a42dc1ba5cbe3b06e
Marcos Federico Mandrille <mmandrille@gmail.com>
2022-08-10 13:08:14 -0300

    first improvements

--- creational/builder.py
+++ creational/builder.py
@@ -11,66 +11,161 @@ only director objects to use the builders ensures control over object creation (
 to the Factory Pattern). 
 
 """
-class HtmlField(object):
-    def __init__(self, **kwargs):
-        self.html = ""
-        if kwargs['field_type'] == "text_field":
-            self.html = self.construct_text_field(kwargs["label"], kwargs["field_name"])
-        elif kwargs['field_type'] == "checkbox":
-            self.html = self.construct_checkbox(kwargs["field_id"], kwargs["value"], kwargs["label"])
+# Python Imports
+import tempfile
+import webbrowser
+from abc import ABCMeta, abstractmethod
 
-    def construct_text_field(self, label, field_name):
-        return '{0}:<br><input type="text" name="{1}"><br>'.format(
-            label,
-            field_name
-        )
+# Classes Definition
+class AbstractDirector(object, metaclass=ABCMeta):
+    def __init__(self):
+        self._builder = None
 
-    def construct_checkbox(self, field_id, value, label):
-        return '<label><input type="checkbox" id="{0}" value="{1}"> {2}<br>'.format(
-            field_id,
-            value,
-            label
-        )
+    def set_builder(self, builder):
+        self._builder = builder
 
-    def __str__(self):
-        return self.html
+    @abstractmethod
+    def construct(self, field_list):
+        pass
+    
+    def get_constructed_object(self):
+        return self._builder.constructed_object
 
-def generate_webform(field_dict_list):
-    generated_field_list = []
-    for field in field_dict_list:
-        try:
-            generated_field_list.append(str(HtmlField(**field)))
-        except Exception as e:
-            print("error: {}".format(e))
+class AbstractFormBuilder(object, metaclass=ABCMeta):
+    def __init__(self):
+        self.constructed_object = None
+    
+    @abstractmethod
+    def add_text_field(self, field_dict):
+        pass
+    
+    @abstractmethod
+    def add_checkbox(self, checkbox_dict):
+        pass
 
-    generated_fields = "\n".join(generated_field_list)
-    return "<form>{fields}</form>".format(fields=generated_fields)
+    @abstractmethod
+    def add_radiogroup(self, radio_dict):
+        pass
 
-def build_html_form(field_list):
-    print(
-        "<html><body>{}</body></html>".format(
-                generate_webform(field_list)
+    @abstractmethod
+    def add_button(self, button_dict):
+        pass
+
+# Builders:
+class HtmlForm(object):
+    def __init__(self):
+        self.field_list = []
+
+    def __repr__(self):
+        return "<form>{}</form>".format("".join(self.field_list))
+
+class HtmlFormBuilder(AbstractFormBuilder):
+    def __init__(self):
+        self.constructed_object = HtmlForm()
+    
+    def add_text_field(self, field_dict):
+        self.constructed_object.field_list.append(
+            '{0}:<br><input type="text" name="{1}"><br>'.
+            format(
+                field_dict['label'],
+                field_dict['field_name']
             )
-    )
+        )
+    
+    def add_checkbox(self, checkbox_dict):
+        self.constructed_object.field_list.append(
+            '<label><input type="checkbox" id="{0}" value="{1}"> {2}<br>'.
+            format(
+                checkbox_dict['field_id'],
+                checkbox_dict['value'],
+                checkbox_dict['label']
+            )
+        )
 
+    def add_radiogroup(self, radio_dict):
+        self.constructed_object.field_list.append(
+            "<fieldset>{0}{1}</fieldset>".format( # Main Block
+                # Legend
+                '<legend>{0}</legend>'.format(radio_dict["label"]),
+                # every value is a item
+                "".join(
+                    [
+                        '<label><input type="radio" name="{0}" value="{1}"> {2}</label>'
+                        .format(
+                            radio_dict["field_name"],
+                            value["value"],
+                            value["label"]
+                        )
+                        for value in radio_dict['values'] # List compression to generate every radio item
+                    ]
+                )
+            )
+        )
+
+    def add_button(self, button_dict):
+        self.constructed_object.field_list.append(
+            '<button type="button">{}</button>'.
+            format(
+                button_dict['text']
+            )
+        )
+            
+
+class FormDirector(AbstractDirector):
+    def __init__(self):
+        AbstractDirector.__init__(self)
+
+    def construct(self, field_list):
+        for field in field_list:
+            if field["field_type"] == "text_field":
+                self._builder.add_text_field(field)
+            elif field["field_type"] == "checkbox":
+                self._builder.add_checkbox(field)
+            elif field["field_type"] == "radio_group":
+                self._builder.add_radiogroup(field)
+            elif field["field_type"] == "button":
+                self._builder.add_button(field)       
+ 
+# Execution
 if __name__ == "__main__":
+    director = FormDirector()
+    # Define Fields
     field_list = [
         {
             "field_type": "text_field",
-            "label": "Best text you have ever written",
-            "field_name": "Field One"
+            "label": "Put some text here",
+            "field_name": "FieldOne"
         },
         {
             "field_type": "checkbox",
             "field_id": "check_it",
             "value": "1",
-            "label": "Check for on",
+            "label": "CheckBox",
         },
         {
-            "field_type": "text_field",
-            "label": "Another Text field",
-            "field_name": "Field One"
+            "field_type": "radio_group",
+            "field_name": "pickone",
+            "label": "Pick One",
+            "values": [
+                {"value": 1, "label": "Option 1"},
+                {"value": 2, "label": "Option 2"},
+                {"value": 3, "label": "Option 3"},
+            ]
+        },
+        {
+            "field_type": "button",
+            "text": "DONE"
         }
     ]
-    
-    build_html_form(field_list)
+    html_form_builder = HtmlFormBuilder()
+    director.set_builder(html_form_builder)
+    director.construct(field_list)
+    with tempfile.NamedTemporaryFile('w', delete=False, suffix='.html') as f:
+        url = 'file://' + f.name
+        f.write(
+            "<html><body>{0!r}</body></html>".
+                format(
+                    director.get_constructed_object()
+                )
+        )
+        webbrowser.open(url)
