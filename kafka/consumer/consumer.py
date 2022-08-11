c75c34fe9f4fa2844d6bdb6fbdf1787669b158a1
Marcos Federico Mandrille <mmandrille@gmail.com>
2022-08-10 23:06:59 -0300

    functional consumer

--- kafka/consumer/consumer.py
+++ kafka/consumer/consumer.py
@@ -6,69 +6,75 @@ import asyncio
 import logging
 from logging import handlers
 from datetime import datetime
+# Package Imports
+from kafka.errors import KafkaError
 #Proyect imports
-from utils.classes import StatusClass
 from utils import constants as zconsts
 from utils import functions as zfuncs
 
+# Avoid Kafka Flooding
+kafka_logger = logging.getLogger('kafka')
+kafka_logger.setLevel(logging.WARN)
 
 #Logging definition:
 rotatorHandler = handlers.RotatingFileHandler(
     filename='logs/certprocessor.log', 
-    mode='a',
+    mode='w', # This will start a clean log file
     maxBytes=10*1024*1024,
-    backupCount=5,
-    encoding=None,
-    delay=0
+    backupCount=1,
+    delay=False
 )
+
 logging.basicConfig(
     level=zconsts.LOGGING_LEVEL,#Change to DEBUG for more info in the constants
-    format='%(asctime)s;%(process)s;%(name)s;%(levelname)s;%(message)s',
+    format='[%(asctime)s;%(process)s;%(name)s;%(levelname)s] %(message)s',
     datefmt="%Y%m%d;%H:%M:%S",
-    handlers=[rotatorHandler, ],
+    handlers=[rotatorHandler, logging.StreamHandler()],
 )
-logger = logging.getLogger("certprocessor.MainCode")
 
+logger = logging.getLogger("Main")
 #Definition of process
-async def process_msgs(status):
+async def process_msgs(process_id):
     # Monitoring
-    logger.info('queue_reader started...')
+    logger.info('queue_reader-%s started...', process_id)
     count=0
     check_time = datetime.now()                        
     #We start reading messages from kafka:
+    kafka_consumer = zfuncs.create_consumer()
     while True:
         try:
-            # Read from Kafka Topic
-            msg = status.kafka_consumer.poll(timeout=1.0)
-            # Check message
+            # We fetch a msg from kafka topic
+            msg = kafka_consumer.poll(timeout_ms=0.1)
             if msg:
-                # We fetch a msg from kafka topic
                 asyncio.sleep(zconsts.MSG_PROCESING_DELAY_MS/1000) # Simulate async slow task
                 #some data sent to screen so we know is working xD
                 if zconsts.SCREENING:
                     count+=1
                     if count > zconsts.SCREENING_RATE:
-                        logger.info("{0} Readed from Topic:{1}. Processed {2} in {3} secs",
-                                    msg["message"],
-                                    zconsts.KAFKA_TOPIC,
-                                    count,
-                                    (datetime.now() - check_time).total_seconds()
-                                )
+                        logger.info(
+                            "%s Readed from Topic:%s. Processed %s in %s secs",
+                            msg.value["message"],
+                            zconsts.KAFKA_TOPIC,
+                            count,
+                            (datetime.now() - check_time).total_seconds()
+                        )
                         #Restart lap
                         count, check_time = 0, datetime.now()
+            else:
+                asyncio.sleep(zconsts.SLEEP_TIME)
 
-            else: # Kafka topic is empty
-                await asyncio.sleep(zconsts.SLEEP_TIME) # we make time of cpu for others process
+        except KafkaError as e:
+            logger.warning('\nKafka Error: %s', e)
+
         except Exception as e:
-            logger.error('Uknown Fail receiving msg from kafka: %s', e, exc_info=sys.exc_info())
+            logger.error("\nUknown Fail receiving msg from kafka: %s", e, exc_info=sys.exc_info())
 
 def main():
-    status = StatusClass() # Instance Global Administrator
     #Instance general loop
     loop = asyncio.get_event_loop()
     #We create async tasks
     for x in range(zconsts.MAX_TASKS):
-        loop.create_task(process_msgs(status))
+        loop.create_task(process_msgs(x))
         logger.info('{0}/{1} tasks created...'.format(x, zconsts.MAX_TASKS))
     #We make the work generator start
     loop.run_forever()
