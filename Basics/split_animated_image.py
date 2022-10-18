2fba434d314ce329f4595144b36734d480ab657b
Marcos Federico Mandrille <marcos.mandrille@zimperium.com>
2022-10-18 11:46:31 -0300

    Update split_animated_image.py

--- Basics/split_animated_image.py
+++ Basics/split_animated_image.py
@@ -7,14 +7,22 @@ def get_images_from_url(url):
     images = []
     response = requests.get(url)
     imageObject = Image.open(BytesIO(response.content))
-    if imageObject.is_animated:
-        for frame in range(0,imageObject.n_frames):
-            imageObject.seek(frame)
-            images.append(imageObject)
+    try:
+        if imageObject.is_animated:
+            for frame in range(0, imageObject.n_frames):
+                imageObject.seek(frame)
+                images.append(imageObject)
 
-    return images
+        return images
+
+    except Exception:
+        return [imageObject, ]
 
 
 images = get_images_from_url('https://storage.googleapis.com/infoxel-tagx-thumbs/Portales/Ads/ad_2022-10-18T14%3A00%3A30_300_600.png')
+for image in images:
+    image.show()
+
+images = get_images_from_url('http://c.files.bbci.co.uk/48DD/production/_107435681_perro1.jpg')
 for image in images:
     image.show()
