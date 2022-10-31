9d9826aa0b236446148e5265d1da67f99b9264f6
Marcos Federico Mandrille <marcos.mandrille@zimperium.com>
2022-10-31 15:19:51 -0300

    Update class-static_methods.py

--- Basics/class-static_methods.py
+++ Basics/class-static_methods.py
@@ -3,18 +3,18 @@ class ClassGrades:
     def __init__(self, grades):
         self.grades = grades
 
-    @classmethod # This method has access to the class
+    @classmethod  # This method has access to the class
     def from_csv(cls, grade_csv_str):
         grades = map(int, grade_csv_str.split(', '))
-        cls.validate(grades) # We use the class static method here
+        cls.validate(grades)  # We use the class static method here
         return cls(grades)
 
-
-    @staticmethod # This function dont has access to the class
+    @staticmethod  # This function dont has access to the class
     def validate(grades):
         for g in grades:
             if g < 0 or g > 100:
                 raise Exception()
+
 
 # Run our code
 if __name__ == "__main__":
@@ -26,5 +26,6 @@ if __name__ == "__main__":
         # Should fail with invalid grades
         class_grades_invalid = ClassGrades.from_csv('92, -15, 99, 101, 77, 65, 100')
         print(class_grades_invalid.grades)
-    except:
+
+    except Exception:
         print('Invalid!')
