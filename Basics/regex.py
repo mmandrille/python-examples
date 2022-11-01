331f85696e9b3e2f6758593e480c0b01d26e0be6
Marcos Federico Mandrille <marcos.mandrille@zimperium.com>
2022-11-01 00:49:43 -0300

    Update regex.py

--- Basics/regex.py
+++ Basics/regex.py
@@ -6,28 +6,28 @@ We are going to make some examples to use regular expressions
 
 CheatSheet:
     ^ asserts position at start of the string
-    
+
     \d matches a digit (equivalent to [0-9])
     \d matches a digit (equivalent to [0-9])
-    
+
     (?P<foo>...) Name capture group foo for ... definition
-    
+
     | alternatives
-    
+
     $ asserts position at the end of the string
-
-
 """
 # Requiered imports
 import re
 
+
 def get_TimeUnits(time_string):
     '''
-    Given a time in HH:MM:SS(AM/PM) format, fetch every one of the items in a different Variable.
+    Given a time in HH:MM:SS(AM/PM) format,
+    fetch every one of the items in a different Variable.
     '''
     # Generate Pattern
     pattern = r"^(?P<hour>\d{2}):(?P<minutes>\d{2}):(?P<seconds>\d{2})(?P<meridian>AM|PM)$"
-    match = re.match(pattern, time_string) # Run match
+    match = re.match(pattern, time_string)  # Run match
     # Extract data if possible
     if match:
         return (
@@ -37,10 +37,11 @@ def get_TimeUnits(time_string):
             match.group("meridian")
         )
 
+
 if __name__ == "__main__":
     timestring = "10:01:45AM"
     tup = get_TimeUnits(timestring)
-    assert 10 == tup[0] # Hours
-    assert 1 == tup[1] # Minutes
-    assert 45 == tup[2] # Seconds
+    assert 10 == tup[0]  # Hours
+    assert 1 == tup[1]  # Minutes
+    assert 45 == tup[2]  # Seconds
     assert 'AM' == tup[3]
