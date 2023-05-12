91b8717cd1c2e02b81f996e62bb2e5d9668858c0
Marcos Federico Mandrille <marcos.mandrille@zimperium.com>
2023-05-12 12:10:59 -0300

    initial test of yolov5

---
+++ yolov5/simple_detect.py
@@ -0,0 +1,24 @@
+import logging
+import torch
+
+# Global
+logger = logging.getLogger(__name__)
+
+
+# Model
+model = torch.hub.load('ultralytics/yolov5', 'yolov5s')
+model.conf = 0.30  # NMS confidence threshold
+model.iou = 0.45  # NMS IoU threshold
+model.agnostic = False  # NMS class-agnostic
+model.multi_label = False  # NMS multiple labels per box
+model.classes = [0]  # (optional list) filter by class, i.e. = [0, 15, 16]
+model.max_det = 1000  # maximum number of detections per image
+model.amp = False  # Automatic Mixed Precision (AMP) inference
+
+# Image
+im = 'https://ultralytics.com/images/zidane.jpg'
+
+# Inference
+results = model(im)
+logger.info(results.pandas().xyxy[0])
+results.show()
